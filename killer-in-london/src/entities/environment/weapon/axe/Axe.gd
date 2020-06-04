1e00e90aa92d6e82b1193dfc6b50f5ce5e6e87dd
JillyMan <Artsiom_Dubinevich@epam.com>
2020-06-04 20:32:49 +0300

    refactoring of custom fsm

--- killer-in-london/src/entities/environment/weapon/axe/Axe.gd
+++ killer-in-london/src/entities/environment/weapon/axe/Axe.gd
@@ -2,7 +2,13 @@ extends KinematicBody2D
 
 var was_throwed : bool = false
 
+export var throw_speed : float = 2000
+export var rotate_speed : float = 100
+
+export var comeback_speed : float = 100
+
 signal on_weapon_returned
+signal need_try_again
 
 func was_throwed() -> bool:
 	return was_throwed
@@ -10,15 +16,11 @@ func was_throwed() -> bool:
 func throw(target : Vector2):
 	if not was_throwed:
 		was_throwed = true
-		$FSM.set_state($FSM/Throwing, true, { 
-			"target": target,
-		})
+		$FSM.set_throwing_state(target)
 
 func comeback(owner : Node2D):
 	if was_throwed:
-		$FSM.set_state($FSM/Comeback, true, { 
-			"come_to": owner,
-		})
+		$FSM.set_comback_state(owner)
 
 func _weapon_returned():
 	assert(was_throwed)
--- killer-in-london/src/entities/environment/weapon/axe/state/Throwing.gd
+++ killer-in-london/src/entities/environment/weapon/axe/state/Throwing.gd
@@ -2,11 +2,6 @@ extends State
 
 const MIN_DISTANCE_BTW_AXE_AND_THROW_DEST = 5
 
-export var throw_speed : float = 1500
-export var rotate_speed : float = 100
-
-onready var idle_state = fsm.get_node("Idle")
-
 var player : Node2D = null
 var throwable_obj : KinematicBody2D = null
 
@@ -19,13 +14,13 @@ var throw_len : float = 0.0
 func fixed_tick(delta): 
 	var length_to_throw_dest = (throwable_obj.global_position - target).length()
 	var friction = length_to_throw_dest / throw_len
-	var linear_velocity = move_dir * throw_speed * friction
+	var linear_velocity = move_dir * host.throw_speed * friction
 	var col = throwable_obj.move_and_collide(linear_velocity * delta, false, true, false)
-	throwable_obj.rotate(delta * rotate_speed * friction)
+	throwable_obj.rotate(delta * host.rotate_speed * friction)
 
 	if (col != null or length_to_throw_dest < MIN_DISTANCE_BTW_AXE_AND_THROW_DEST):
 		was_throwed = false
-		fsm.set_state(idle_state)
+		fsm.set_idle_state()
 
 func enter(payload):
 	assert(host is KinematicBody2D)
