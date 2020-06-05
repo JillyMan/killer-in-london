1a5c370a639273347b9e5eca51894639cac835ac
JillyMan <Artsiom_Dubinevich@epam.com>
2020-06-05 15:05:27 +0300

    restrict of throw distance.

---
+++ killer-in-london/src/entities/environment/weapon/axe/FSMAxe.gd
@@ -0,0 +1,19 @@
+class_name AxeFSM
+extends FSM
+
+onready var IdleState : State = get_node("Idle")
+onready var ThrowingState : State = get_node("Throwing")
+onready var ComebackState : State = get_node("Comeback")
+
+func set_throwing_state(target: Vector2):
+	set_state(ThrowingState, true, { 
+		"target": target,
+	})
+
+func set_idle_state():
+	set_state(IdleState)
+
+func set_comback_state(owner : Node2D):
+	set_state(ComebackState, true, { 
+		"come_to": owner,
+	})
