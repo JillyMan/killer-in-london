aae7eab2941f62c2cb176e5a2ebdb8c0a021e9b4
JillyMan <Artsiom_Dubinevich@epam.com>
2020-06-10 15:29:57 +0300

    fix bug

--- killer-in-london/src/entities/environment/weapon/axe/state/Idle.gd
+++ killer-in-london/src/entities/environment/weapon/axe/state/Idle.gd
@@ -1,9 +1,7 @@
 extends State
 
 func enter(payload):
-	print("entered")
 	host.damage_producer_shape.set_deferred("disabled", true)
 
 func exit(payload):
-	print("exited")	
 	host.damage_producer_shape.set_deferred("disabled", false)
